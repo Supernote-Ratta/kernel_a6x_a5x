146fe8856bbe5a3e2ea51d1c29fb0bc30594eb4c
Jason Tian <tianshengjie@supernote.com>
2020-07-27 20:36:17 +0800

    Update version to debug-20200727a

--- fs/proc/proc_ratta.c
+++ fs/proc/proc_ratta.c
@@ -9,7 +9,7 @@
 
 #include <linux/proc_ratta.h>
 
-#define RATTA_KERNEL_VERSION "debug-20200724a"
+#define RATTA_KERNEL_VERSION "debug-20200727a"
 
 static int bootmode = 0;
 static int volatile pen_type = 0;
