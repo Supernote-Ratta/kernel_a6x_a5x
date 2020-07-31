cbf8ba473ae70e922a0b0cb4b993e1ba7c7d41de
Neo <houyong@supernote.com>
2020-07-31 14:33:52 +0800

    fs: proc: ratta: Update version to release-20200731a

--- fs/proc/proc_ratta.c
+++ fs/proc/proc_ratta.c
@@ -9,7 +9,7 @@
 
 #include <linux/proc_ratta.h>
 
-#define RATTA_KERNEL_VERSION "release-20200730a"
+#define RATTA_KERNEL_VERSION "release-20200731a"
 
 static int bootmode = 0;
 static int volatile pen_type = 0;
